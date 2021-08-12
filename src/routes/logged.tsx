10d26037384164058d533becf36a3b289bb2b5bd
moronkids <sulistyoardani@gmail.com>
2021-08-12 13:02:13 +0700

    fix: click outside

--- src/routes/logged.tsx
+++ src/routes/logged.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 // import { Component } from "react";
 import { Route, Redirect } from "react-router-dom";
 import Wrapper from 'components/layouts'
@@ -7,11 +7,19 @@ import { Hooks } from "providers"
 import Stage from 'components/list-event/parts/popup'
 import SubmitSuccessPopup from 'components/scoring-team/parts/popup'
 const Guest = ({ component: Component, ...rest }) => {
-  const { contactUs, setContactUs } = useContext(Hooks);
+  const [bgActive, setbgActive] = useState(false);
+
+  const { contactUs, setContactUs, sorting, profile, setProfile, setSorting } = useContext(Hooks);
+  useEffect(() => {
+    if (sorting || profile) {
+      setbgActive(true)
+    }
+  }, [sorting, profile])
   const token = localStorage.getItem('token');
   if (token === null) {
     return <Redirect to="/login" />
   }
+
   return (
     <Route
       {...rest}
@@ -19,18 +27,29 @@ const Guest = ({ component: Component, ...rest }) => {
 
         return (
           <>
+            <div className="overlay-transparent" style={{
+              width: '100vw',
+              height: '100vh',
+              zIndex: '9',
+              position: 'fixed',
+              background: 'transparent',
+              display: bgActive ? 'block' : 'none'
+            }}
+              onClick={() => { setbgActive(false); setProfile(false); setSorting(false) }}></div>
             <ContactUs />
             <Stage />
             <SubmitSuccessPopup />
-            {rest.header === undefined ? (
-              <Wrapper ishelp={rest.ishelp}>
-                <Component {...props} />
-              </Wrapper>
-            ) : (
-              <Wrapper headers={'off'}>
-                <Component {...props} />
-              </Wrapper>
-            )}
+            {
+              rest.header === undefined ? (
+                <Wrapper ishelp={rest.ishelp}>
+                  <Component {...props} />
+                </Wrapper>
+              ) : (
+                <Wrapper headers={'off'}>
+                  <Component {...props} />
+                </Wrapper>
+              )
+            }
           </>
         );
       }}
